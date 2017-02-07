f636c9adc03205b51b6e47b836f84f2b6f860db0
Nick P <nb.piscitello@gmail.com>
2017-02-07 15:08:54 -0500

    simple 'turn everything on' sketch works, proves hardware

--- dash/dash.ino
+++ dash/dash.ino
@@ -1,9 +1,22 @@
+// To talk to the MAX7221 driver
+#include "LedControl.h"
+
+/*  Set up MAX7221 object
+ *  pin 12: data
+ *  pin 13: clock
+ *  pin 11: chip select
+ *  There is only one MAX7221 attached */
+LedControl dashboard(12, 13, 11, 1);
+
 void setup() {
-  // put your setup code here, to run once:
-
+  // wake up the chip and increase brightness
+  dashboard.shutdown(0, false);
+  dashboard.setIntensity(0, 8);
+  
+  // turn on every LED
+  for( int i = 0; i < 8; i++ ) {
+    dashboard.setRow(0, i, 0xFF);
+  }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
-}
+void loop() {}
