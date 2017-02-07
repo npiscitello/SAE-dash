55432e5d01e9f87c2cb84fb7340547b9aa37bb24
Nick P <nb.piscitello@gmail.com>
2017-02-07 15:58:57 -0500

    cool hardware test working - I screwed up, so I had to rewrite from scratch and figured I'd change the name

---
+++ hardware_test/hardware_test.ino
@@ -0,0 +1,48 @@
+// To talk to the MAX7221 driver
+#include "LedControl.h"
+
+/*  Set up MAX7221 object
+ *  pin 12: data
+ *  pin 13: clock
+ *  pin 11: chip select
+ *  There is only one MAX7221 attached */
+LedControl dashboard(12, 13, 11, 1);
+
+// what to write to the LED row
+uint8_t led = 0x00;
+
+void setup() {
+  // wake up the chip and increase brightness
+  dashboard.shutdown(0, false);
+  dashboard.setIntensity(0, 8);
+}
+
+void loop() {
+  // turn on every LED in order
+  for(int i = 0; i < 8; i++) {
+    led = 0x00;
+    for(int j = 0; j < 8; j++) {
+      led = (led >> 1) | 0x80;
+      dashboard.setRow(0, i, led);
+      delay(75);
+    }
+  }
+
+  // allow all to revel in the glory of the One True Dashboard
+  delay(2000);
+
+  // turn off every LED in order
+  for(int i = 7; i >= 0; i--) {
+    led = 0xFF;
+    for(int j = 7; j >= 0; j--) {
+      led = led << 1;
+      dashboard.setRow(0, i, led);
+      delay(75);
+    }
+  }
+
+  // allow all to quiver in antici...
+  // ...pation!
+  delay(2000);
+}
+
